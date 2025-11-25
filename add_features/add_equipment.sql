b0a8f49baec3e7645cfffa81e9cecfcb3459832f
Tawan <chawinthornkittivacharaphong@Chawinthorns-MacBook-Pro.local>
2025-11-26 00:49:52 +0700

    fix: Removed part_number already have serial number.

--- add_features/add_equipment.sql
+++ add_features/add_equipment.sql
@@ -1,4 +1,4 @@
-create function add_equipment(p_company_id integer, p_part_number character varying, p_name character varying, p_serial_number character varying, p_purchase_cost numeric, p_last_service_date date, p_category character varying, p_quantity integer, p_next_service_date date, p_status character varying) returns integer
+create function add_equipment(p_company_id integer, p_name character varying, p_serial_number character varying, p_purchase_cost numeric, p_last_service_date date, p_category character varying, p_quantity integer, p_next_service_date date, p_status character varying) returns integer
     language plpgsql
 as
 $$
@@ -6,12 +6,12 @@ DECLARE
     new_id INT;
 BEGIN
     INSERT INTO "equipment" (
-        company_id, part_number, name, serial_number,
+        company_id, name, serial_number,
         purchase_cost, last_service_date, category,
         quantity, next_service_data, status
     )
     VALUES (
-               p_company_id, p_part_number, p_name, p_serial_number,
+               p_company_id, p_name, p_serial_number,
                p_purchase_cost, p_last_service_date, p_category,
                p_quantity, p_next_service_date,
                COALESCE(p_status, 'active') -- Use provided status or default to 'active'
@@ -22,5 +22,5 @@ BEGIN
 END;
 $$;
 
-alter function add_equipment(integer, varchar, varchar, varchar, numeric, date, varchar, integer, date, varchar) owner to sc_db_project_user;
+alter function add_equipment(integer, varchar, varchar, numeric, date, varchar, integer, date, varchar) owner to sc_db_project_user;
 
