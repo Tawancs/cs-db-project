9663739a63342e647d9053299c52a5a2833ab436
Tawan <chawinthornkittivacharaphong@Chawinthorns-MacBook-Pro.local>
2025-11-25 23:46:19 +0700

    feat: Added show subscription feature.

---
+++ show_subscription.sql
@@ -0,0 +1,13 @@
+-- auto-generated definition
+create function show_subscription()
+    returns TABLE(name character varying, detail character varying, rank character varying, price numeric)
+    language plpgsql
+as
+$$
+begin
+    -- missing source code
+end;
+$$;
+
+alter function show_subscription() owner to sc_db_project_user;
+
