867e3717de15d9826614c5baf8f112783832925d
Tawan <chawinthornkittivacharaphong@Chawinthorns-MacBook-Pro.local>
2025-11-26 00:40:10 +0700

    fix: Removed retail_price from equipment.

--- add_features/add_equipment.sql
+++ add_features/add_equipment.sql
@@ -1,5 +1,4 @@
--- auto-generated definition
-create function add_equipment(p_company_id integer, p_part_number character varying, p_name character varying, p_serial_number character varying, p_purchase_cost numeric, p_retail_price numeric, p_last_service_date date, p_category character varying, p_quantity integer, p_next_service_date date, p_status character varying) returns integer
+create function add_equipment(p_company_id integer, p_part_number character varying, p_name character varying, p_serial_number character varying, p_purchase_cost numeric, p_last_service_date date, p_category character varying, p_quantity integer, p_next_service_date date, p_status character varying) returns integer
     language plpgsql
 as
 $$
@@ -8,12 +7,12 @@ DECLARE
 BEGIN
     INSERT INTO "equipment" (
         company_id, part_number, name, serial_number,
-        purchase_cost, retail_price, last_service_date, category,
+        purchase_cost, last_service_date, category,
         quantity, next_service_data, status
     )
     VALUES (
                p_company_id, p_part_number, p_name, p_serial_number,
-               p_purchase_cost, p_retail_price, p_last_service_date, p_category,
+               p_purchase_cost, p_last_service_date, p_category,
                p_quantity, p_next_service_date,
                COALESCE(p_status, 'active') -- Use provided status or default to 'active'
            )
@@ -23,5 +22,5 @@ BEGIN
 END;
 $$;
 
-alter function add_equipment(integer, varchar, varchar, varchar, numeric, numeric, date, varchar, integer, date, varchar) owner to sc_db_project_user;
+alter function add_equipment(integer, varchar, varchar, varchar, numeric, date, varchar, integer, date, varchar) owner to sc_db_project_user;
 
