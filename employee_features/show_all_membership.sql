2f326d46f4185bf27d82771f0abb17fd0a46bf56
Tawan <chawinthornkittivacharaphong@Chawinthorns-MacBook-Pro.local>
2025-11-27 20:25:39 +0700

    fix: Added contact info.

--- employee_features/show_all_membership.sql
+++ employee_features/show_all_membership.sql
@@ -1,24 +1,34 @@
 create function show_all_membership(p_company_id integer)
-    returns TABLE(customer_name text, membership_name character varying, price numeric, expired_date date, status text)
+    returns TABLE(customer_name text, email character varying, phone character varying, type character varying, start_date date, expiry_date date, days_until_expiry text, status text)
     language plpgsql
 as
 $$
 BEGIN
     RETURN QUERY
         SELECT
-            (c.first_name || ' ' || c.last_name)::TEXT AS customer_name,
+            (c.first_name || ' ' || c.last_name)::TEXT,
+            c.email,
+            c.telephone_no,
             m.name,
-            m.price,
+            m.purchased_date,
             m.expired_date,
             CASE
-                WHEN m.expired_date < CURRENT_DATE THEN 'Expired'
-                WHEN (m.expired_date - CURRENT_DATE) <= 30 THEN 'Expiring Soon'
-                ELSE 'Active'
-                END::TEXT AS status
-        FROM membership AS m
-                 JOIN customer AS c ON c.member_id = m.id
+                WHEN m.expired_date < CURRENT_DATE THEN
+                    'Expired ' || (CURRENT_DATE - m.expired_date)::TEXT || ' days ago'
+                WHEN m.expired_date = CURRENT_DATE THEN
+                    'Expires today'
+                ELSE
+                    (m.expired_date - CURRENT_DATE)::TEXT || ' days left'
+                END::TEXT,
+            CASE
+                WHEN m.expired_date < CURRENT_DATE THEN 'EXPIRED'
+                ELSE 'ACTIVE'
+                END::TEXT
+
+        FROM "customer" c
+                 JOIN "membership" m ON c.member_id = m.id
         WHERE m.company_id = p_company_id
-        ORDER BY m.rank DESC;
+        ORDER BY m.expired_date DESC;
 END;
 $$;
 
