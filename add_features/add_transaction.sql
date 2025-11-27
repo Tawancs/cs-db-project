0c4e547dceda9fe2bd3291312b6b102e3cc17ee3
Tawan <chawinthornkittivacharaphong@Chawinthorns-MacBook-Pro.local>
2025-11-27 20:26:06 +0700

    fix: used appointment_id and service_id instead.

--- add_features/add_transaction.sql
+++ add_features/add_transaction.sql
@@ -1,27 +1,21 @@
--- auto-generated definition
-create function add_transaction(p_company_id integer, p_customer_id integer, p_service_id integer, p_amount numeric, p_currency character varying, p_payment_method character varying, p_reference_id character varying, p_type character varying, p_status character varying) returns integer
+create function add_transaction(p_appointment_id integer, p_service_id integer, p_amount numeric, p_payment_method character varying, p_reference_id character varying, p_description text, p_currency character varying DEFAULT 'THB'::character varying) returns integer
     language plpgsql
 as
 $$
 DECLARE
-    new_transaction_id INT;
+    new_transaction_id INTEGER;
 BEGIN
     INSERT INTO "transaction" (
-        company_id, customer_id, service_id,
-        amount, currency, payment_method, reference_id, type, status
-    )
-    VALUES (
-               p_company_id, p_customer_id, p_service_id,
-               p_amount,
-               COALESCE(p_currency, 'THB'), -- Use input or default to THB
-               p_payment_method, p_reference_id, p_type,
-               COALESCE(p_status, 'pending') -- Use input or default to pending
-           )
-    RETURNING id INTO new_transaction_id;
+        appointment_id, service_id, amount, payment_method,
+        reference_id, description, currency
+    ) VALUES (
+                 p_appointment_id, p_service_id, p_amount, p_payment_method,
+                 p_reference_id, p_description, p_currency
+             ) RETURNING transaction_id INTO new_transaction_id;
 
     RETURN new_transaction_id;
 END;
 $$;
 
-alter function add_transaction(integer, integer, integer, numeric, varchar, varchar, varchar, varchar, varchar) owner to sc_db_project_user;
+alter function add_transaction(integer, integer, numeric, varchar, varchar, text, varchar) owner to sc_db_project_user;
 
