5c4b905fafe0d69f6ec29e53e7f53669cd116cfe
Tawan <chawinthornkittivacharaphong@Chawinthorns-MacBook-Pro.local>
2025-11-27 17:12:27 +0700

    feat: Added show all membership for employee to check whether it already expired or not.

---
+++ employer_features/show_all_membership.sql
@@ -0,0 +1,25 @@
+create function show_all_membership(p_company_id integer)
+    returns TABLE(customer_name text, membership_rank character varying, price numeric, expired_date date, status text)
+    language plpgsql
+as
+$$
+BEGIN
+    RETURN QUERY
+        SELECT
+            (c.first_name || ' ' || c.last_name)::TEXT AS customer_name,
+            m.rank,
+            m.price,
+            m.expired_date,
+            CASE
+                WHEN m.expired_date < CURRENT_DATE THEN 'Expired'
+                WHEN (m.expired_date - CURRENT_DATE) <= 30 THEN 'Expiring Soon'
+                ELSE 'Active'
+                END::TEXT AS status
+        FROM membership AS m
+                 JOIN customer AS c ON c.member_id = m.id
+        WHERE m.company_id = p_company_id;
+END;
+$$;
+
+alter function show_all_membership(integer) owner to sc_db_project_user;
+
