147f93076b5db58c7c0f08f972a5d42b3aa9b877
Tawan <chawinthornkittivacharaphong@Chawinthorns-MacBook-Pro.local>
2025-11-28 04:35:26 +0700

    feat: Added add employee schedule.

--- add_features/add_employee_schedule.sql
+++ add_features/add_employee_schedule.sql
@@ -1,15 +1,17 @@
-create function add_employee_schedule(p_employee_id integer, p_date date, p_start_time time without time zone, p_end_time time without time zone, p_status character varying) returns integer
+create function add_employee_schedule(p_employee_id integer,
+                                      p_date date,
+                                      p_start_time time without time zone,
+                                      p_end_time time without time zone,
+                                      p_status character varying) returns integer
     language plpgsql
 as
 $$
 DECLARE
     v_id INTEGER;
 BEGIN
-    INSERT INTO employee_schedule (
-        employee_id, date, start_time, end_time, status
-    ) VALUES (
-                 p_employee_id, p_date, p_start_time, p_end_time, p_status
-             ) RETURNING schedule_id INTO v_id;
+    INSERT INTO employee_schedule (employee_id, date, start_time, end_time, status)
+    VALUES (p_employee_id, p_date, p_start_time, p_end_time, p_status)
+    RETURNING schedule_id INTO v_id;
     RETURN v_id;
 END;
 $$;
