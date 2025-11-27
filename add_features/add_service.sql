10b5cdbc7971f39e584648f5ca65fe409c9cebf3
Tawan <chawinthornkittivacharaphong@Chawinthorns-MacBook-Pro.local>
2025-11-27 13:30:00 +0700

    fix: modify service features.

--- add_features/add_service.sql
+++ add_features/add_service.sql
@@ -1,25 +1,16 @@
--- auto-generated definition
-create function add_service(p_company_id integer, p_appointment_id integer, p_lead_mechanic_id integer, p_type character varying, p_cost integer, p_duration integer, p_received_date date, p_expected_finish_date date) returns integer
+create function add_service(p_company_id integer, p_name character varying, p_detail character varying, p_approximate_duration integer, p_min_cost numeric, p_max_cost numeric) returns integer
     language plpgsql
 as
 $$
 DECLARE
-    new_service_id INT;
+    v_id INTEGER;
 BEGIN
-    INSERT INTO "service" (
-        company_id, appointment_id, lead_mechanic_id,
-        type, cost, duration, received_date, expected_finish_date
-        -- status defaults to 'in progress'
-    )
-    VALUES (
-               p_company_id, p_appointment_id, p_lead_mechanic_id,
-               p_type, p_cost, p_duration, p_received_date, p_expected_finish_date
-           )
-    RETURNING id INTO new_service_id;
-
-    RETURN new_service_id;
+    INSERT INTO "service" (company_id, name, detail, approximate_duration, min_cost, max_cost)
+    VALUES (p_company_id, p_name, p_detail, p_approximate_duration, p_min_cost, p_max_cost)
+    RETURNING id INTO v_id;
+    RETURN v_id;
 END;
 $$;
 
-alter function add_service(integer, integer, integer, varchar, integer, integer, date, date) owner to sc_db_project_user;
+alter function add_service(integer, varchar, varchar, integer, numeric, numeric) owner to sc_db_project_user;
 
