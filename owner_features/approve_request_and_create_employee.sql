4450ca7a960bae1a8544f7230e6bb9ae3d0f11da
Tawan <chawinthornkittivacharaphong@Chawinthorns-MacBook-Pro.local>
2025-11-29 00:42:10 +0700

    feat: Added approve feature for recruitment.

--- owner_features/approve_request_and_create_employee.sql
+++ owner_features/approve_request_and_create_employee.sql
@@ -1,20 +1,31 @@
-create function approve_request_and_create_employee(p_email character varying, p_salary numeric, p_contract_type character varying, p_contract_period character varying) returns integer
+create function approve_request_and_create_employee(p_email character varying, p_salary numeric,
+                                                    p_contract_type character varying,
+                                                    p_contract_period character varying) returns integer
     language plpgsql
 as
 $$
 DECLARE
-    r RECORD;
-    v_new_username VARCHAR;
-    v_new_password VARCHAR;
-    v_employee_id INTEGER;
+r RECORD;
+    v_new_username
+VARCHAR;
+    v_new_password
+VARCHAR;
+    v_employee_id
+INTEGER;
 BEGIN
-    SELECT * INTO r FROM "request_form" WHERE request_form.email = p_email;
+SELECT *
+INTO r
+FROM "request_form"
+WHERE request_form.email = p_email;
 
-    v_new_username := lower(r.first_name || '.' || r.last_name || r.request_id);
+v_new_username
+:= lower(r.first_name || '.' || r.last_name || r.request_id);
     -- Password: Default temporary password
-    v_new_password := 'changeme1234';
+    v_new_password
+:= 'changeme1234';
 
-    v_employee_id := add_employee(
+    v_employee_id
+:= add_employee(
             r.company_id,
             v_new_username,
             r.first_name,
@@ -34,9 +45,11 @@ BEGIN
      );
 
     -- 4. Delete the request (since they are now an employee)
-    DELETE FROM "request_form" WHERE email = p_email;
+DELETE
+FROM "request_form"
+WHERE email = p_email;
 
-    RETURN v_employee_id;
+RETURN v_employee_id;
 END;
 $$;
 
