79633f43336ec7bcffb5c3a0a50e6cc1b80e5c9d
Tawan <chawinthornkittivacharaphong@Chawinthorns-MacBook-Pro.local>
2025-11-29 02:53:59 +0700

    fix: Set status as pending as default.

--- add_features/add_appointment.sql
+++ add_features/add_appointment.sql
@@ -1,22 +1,18 @@
--- auto-generated definition
-create function add_appointment(p_vehicle_id integer, p_details character varying, p_appointment_date timestamp without time zone) returns integer
+create function add_appointment(p_company_id integer, p_vehicle_id integer, p_details character varying,
+                                p_appointment_date timestamp without time zone) returns integer
     language plpgsql
 as
 $$
 DECLARE
-    new_appointment_id INT;
+    new_appointment_id INTEGER;
 BEGIN
-    INSERT INTO "appointment" (
-        vehicle_id, details, appointment_date
-    )
-    VALUES (
-               p_vehicle_id, p_details, p_appointment_date
-           )
+    INSERT INTO "appointment" (company_id, vehicle_id, details, appointment_date, status)
+    VALUES (p_company_id, p_vehicle_id, p_details, p_appointment_date,
+            'pending')
     RETURNING id INTO new_appointment_id;
-
     RETURN new_appointment_id;
 END;
 $$;
 
-alter function add_appointment(integer, varchar, timestamp) owner to sc_db_project_user;
+alter function add_appointment(integer, integer, varchar, timestamp) owner to sc_db_project_user;
 
