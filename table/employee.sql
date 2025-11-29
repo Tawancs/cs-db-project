5ec34da37b9de4debb69ab60d8bd7f741b36f840
Tawan <chawinthornkittivacharaphong@Chawinthorns-MacBook-Pro.local>
2025-11-30 02:30:11 +0700

    fix: wrong file.

--- table/employee.sql
+++ table/employee.sql
@@ -1,23 +1,28 @@
-create table customer
+create table employee
 (
-    id           serial
+    id              serial
         primary key,
-    username     varchar(50)  not null
+    company_id      integer,
+    username        varchar(50)  not null
         unique,
-    display_name varchar(50)  not null,
-    first_name   varchar(50)  not null,
-    middle_name  varchar(50),
-    last_name    varchar(50)  not null,
-    address      varchar(255) not null,
-    city         varchar(100) not null,
-    postal_code  varchar(20)  not null,
-    email        varchar(100) not null
+    first_name      varchar(50)  not null,
+    middle_name     varchar(50),
+    last_name       varchar(50)  not null,
+    address         varchar(255) not null,
+    city            varchar(100) not null,
+    postal_code     varchar(20)  not null,
+    email           varchar(100) not null
         unique,
-    telephone_no varchar(10)  not null,
-    password     varchar(255) not null
+    telephone_no    varchar(10)  not null,
+    specialize      varchar(100) not null,
+    password        varchar(255) not null,
+    salary          numeric(15, 2) default 15000.00,
+    hire_date       date           default CURRENT_DATE,
+    contract_type   varchar(50)    default 'Full-Time'::character varying,
+    contract_period varchar(50)
 );
 
-alter table customer
+alter table employee
     owner to sc_db_project_user;
 
 
