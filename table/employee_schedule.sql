921c9cf86cc8d95980ac734c8a5c22d96ee1268a
Tawan <chawinthornkittivacharaphong@Chawinthorns-MacBook-Pro.local>
2025-11-30 02:30:43 +0700

    fix: wrong file

--- table/employee_schedule.sql
+++ table/employee_schedule.sql
@@ -1,23 +1,17 @@
-create table customer
+create table employee_schedule
 (
-    id           serial
+    schedule_id serial
         primary key,
-    username     varchar(50)  not null
-        unique,
-    display_name varchar(50)  not null,
-    first_name   varchar(50)  not null,
-    middle_name  varchar(50),
-    last_name    varchar(50)  not null,
-    address      varchar(255) not null,
-    city         varchar(100) not null,
-    postal_code  varchar(20)  not null,
-    email        varchar(100) not null
-        unique,
-    telephone_no varchar(10)  not null,
-    password     varchar(255) not null
+    employee_id integer
+        constraint fk_eschedule_employee
+            references employee,
+    date        date         not null,
+    start_time  time         not null,
+    end_time    time         not null,
+    status      varchar(100) not null
 );
 
-alter table customer
+alter table employee_schedule
     owner to sc_db_project_user;
 
 
