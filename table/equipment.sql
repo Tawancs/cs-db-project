f75280bad083f5d246d18b11279cd8240fc0350e
Tawan <chawinthornkittivacharaphong@Chawinthorns-MacBook-Pro.local>
2025-11-30 02:31:07 +0700

    fix: wrong file.

--- table/equipment.sql
+++ table/equipment.sql
@@ -1,23 +1,23 @@
-create table customer
+create table equipment
 (
-    id           serial
+    id                 serial
         primary key,
-    username     varchar(50)  not null
-        unique,
-    display_name varchar(50)  not null,
-    first_name   varchar(50)  not null,
-    middle_name  varchar(50),
-    last_name    varchar(50)  not null,
-    address      varchar(255) not null,
-    city         varchar(100) not null,
-    postal_code  varchar(20)  not null,
-    email        varchar(100) not null
-        unique,
-    telephone_no varchar(10)  not null,
-    password     varchar(255) not null
+    company_id         integer
+        constraint fk_equipment_company
+            references company
+            on delete cascade,
+    name               varchar(50)    not null,
+    serial_number      varchar(20)    not null,
+    purchase_cost      numeric(15, 2) not null,
+    last_service_date  date           not null,
+    category           varchar(20)    not null,
+    quantity           integer     default 1,
+    purchased_date     date        default CURRENT_DATE,
+    next_service_data  date        default CURRENT_DATE,
+    operational_status varchar(20) default 'active'::character varying
 );
 
-alter table customer
+alter table equipment
     owner to sc_db_project_user;
 
 
