fc4da0e1fa4286b377e9ebc14c83ba0a2d1d2b00
Tawan <chawinthornkittivacharaphong@Chawinthorns-MacBook-Pro.local>
2025-11-30 02:31:58 +0700

    fix: Wrong table.

--- table/financial_record.sql
+++ table/financial_record.sql
@@ -1,23 +1,19 @@
-create table customer
+create table financial_record
 (
-    id           serial
+    record_id     serial
         primary key,
-    username     varchar(50)  not null
-        unique,
-    display_name varchar(50)  not null,
-    first_name   varchar(50)  not null,
-    middle_name  varchar(50),
-    last_name    varchar(50)  not null,
-    address      varchar(255) not null,
-    city         varchar(100) not null,
-    postal_code  varchar(20)  not null,
-    email        varchar(100) not null
-        unique,
-    telephone_no varchar(10)  not null,
-    password     varchar(255) not null
+    company_id    integer not null
+        constraint fk_fin_rec_company
+            references company
+            on delete cascade,
+    month         integer not null,
+    year          integer not null,
+    total_revenue numeric(15, 2) default 0.00,
+    net_income    numeric(15, 2) default 0.00,
+    date_reported date           default CURRENT_DATE
 );
 
-alter table customer
+alter table financial_record
     owner to sc_db_project_user;
 
 
