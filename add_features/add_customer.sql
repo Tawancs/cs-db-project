c308ff7ab2112e7f86ccfc21cabfb3b1731a9d6e
Tawan <chawinthornkittivacharaphong@Chawinthorns-MacBook-Pro.local>
2025-11-29 23:00:31 +0700

    fix: remove member id and use customer id instead in case of customer buy membership from 2 garages.

--- add_features/add_customer.sql
+++ add_features/add_customer.sql
@@ -1,4 +1,8 @@
-create function add_customer(p_member_id integer, p_username character varying, p_display_name character varying, p_first_name character varying, p_middle_name character varying, p_last_name character varying, p_address character varying, p_city character varying, p_postal_code character varying, p_email character varying, p_telephone_no character varying, p_password character varying) returns integer
+create function add_customer(p_username character varying, p_display_name character varying,
+                             p_first_name character varying, p_middle_name character varying,
+                             p_last_name character varying, p_address character varying, p_city character varying,
+                             p_postal_code character varying, p_email character varying,
+                             p_telephone_no character varying, p_password character varying) returns integer -- user_id
     language plpgsql
 as
 $$
@@ -6,33 +10,26 @@ DECLARE
     new_customer_id INT;
 BEGIN
     -- Insert the new customer
-    INSERT INTO "customer" (
-        member_id, username, display_name,
-        first_name, middle_name, last_name,
-        address, city, postal_code,
-        email, telephone_no, password
-    )
-    VALUES (
-               p_member_id,
-               p_username,
-               p_display_name,
-               p_first_name,
-               p_middle_name, -- Can be NULL
-               p_last_name,
-               p_address,
-               p_city,
-               p_postal_code,
-               p_email,
-               p_telephone_no,
-               crypt(p_password, gen_salt('bf'))
-           )
+    INSERT INTO "customer" (username, display_name,
+                            first_name, middle_name, last_name,
+                            address, city, postal_code,
+                            email, telephone_no, password)
+    VALUES (p_username,
+            p_display_name,
+            p_first_name,
+            p_middle_name, -- Can be NULL
+            p_last_name,
+            p_address,
+            p_city,
+            p_postal_code,
+            p_email,
+            p_telephone_no,
+            crypt(p_password, gen_salt('bf')))
     RETURNING id INTO new_customer_id;
 
-    -- Return the ID of the new customer
     RETURN new_customer_id;
 
 EXCEPTION
-    -- Catch duplicate username or email errors nicely
     WHEN unique_violation THEN
         RAISE EXCEPTION 'User creation failed: Username or Email already exists.';
 END;
